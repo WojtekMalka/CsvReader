31770d671c81410fa822d1157738644e1f06ff6f
WojtekMalka <malka_wojciech@wp.pl>
2021-08-29 18:12:11 +0200

    H2 database properties

--- src/main/java/pl/WojtekMalka/csvReader/CsvReaderApplication.java
+++ src/main/java/pl/WojtekMalka/csvReader/CsvReaderApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CsvReaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsvReaderApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CsvReaderApplication.class, args);
+    }
 }
