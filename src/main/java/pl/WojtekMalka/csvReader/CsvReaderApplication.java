eac5cbea949ff3130594441b7ccdd2a0fe37d6d3
WojtekMalka <malka_wojciech@wp.pl>
2021-08-30 16:19:28 +0200

    ReaderService simpliest version

--- src/main/java/pl/WojtekMalka/csvReader/CsvReaderApplication.java
+++ src/main/java/pl/WojtekMalka/csvReader/CsvReaderApplication.java
@@ -2,11 +2,15 @@ package pl.WojtekMalka.csvReader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.WojtekMalka.csvReader.service.ReaderService;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class CsvReaderApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(CsvReaderApplication.class, args);
+        ReaderService.readLine();
     }
 }
--- src/main/java/pl/WojtekMalka/csvReader/service/ReaderService.java
+++ src/main/java/pl/WojtekMalka/csvReader/service/ReaderService.java
@@ -1,4 +1,17 @@
 package pl.WojtekMalka.csvReader.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ReaderService {
+
+    private static final String fileName = "C:\\Users\\WojtekM\\Repozytorium\\testInput.csv";
+
+    public static void readLine() throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(System.out::println);
+        }
+    }
 }
