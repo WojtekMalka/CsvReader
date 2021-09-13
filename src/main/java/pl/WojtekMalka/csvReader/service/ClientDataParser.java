e3ad68cf5a550956c21525ef25fcb317ea1dfaae
WojtekMalka <malka_wojciech@wp.pl>
2021-09-13 22:01:50 +0200

    ReaderService changes

---
+++ src/main/java/pl/WojtekMalka/csvReader/service/ClientDataParser.java
@@ -0,0 +1,10 @@
+package pl.WojtekMalka.csvReader.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientDataParser {
+    public static void parse(){
+
+    }
+}
--- src/test/java/pl/WojtekMalka/csvReader/service/ReaderServiceTest.java
+++ src/test/java/pl/WojtekMalka/csvReader/service/ReaderServiceTest.java
@@ -1,27 +1,39 @@
 package pl.WojtekMalka.csvReader.service;
 
 import com.opencsv.exceptions.CsvException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.WojtekMalka.csvReader.dictionary.CsvColumns;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static pl.WojtekMalka.csvReader.service.ReaderService.*;
 
 class ReaderServiceTest {
+
+    @BeforeEach
+    public void init() {
+        ReaderService readerService = new ReaderService();
+    }
 
     @Test
     void shouldThrowNullPointerWhenTryReadFile() throws IOException, CsvException {
         assertThrows(
                 NullPointerException.class,
-                () -> ReaderService.readFile(null));
+                () -> readFile(null));
     }
 
     @Test
-    void shouldReadfirstLine() throws IOException, CsvException {
-        List<String[]> sourceFile = ReaderService.readFile("C:\\Users\\WojtekM\\Repozytorium\\testInput.csv");
-        assertEquals(Arrays.asList("Stefan", "Testowy", "1988.11.11", "600700800"), ReaderService.oneReadedLine(sourceFile, 0));
+    void shouldReadFirstLine() throws IOException, CsvException {
+        List<String[]> sourceFile = readFile("C:\\Users\\WojtekM\\Repozytorium\\testInput.csv");
+        assertEquals(Arrays.asList("Stefan", "Testowy", "1988.11.11", "600700800"), readOneLine(sourceFile, 0));
+    }
+
+    @Test
+    void shouldParseFileToMap() {
+
     }
 }
