98e18da495e4ddaa54e0d857df9e6ca15e0d108c
WojtekMalka <malka_wojciech@wp.pl>
2021-09-20 23:03:39 +0200

    Parser and logger in Reader Service

--- src/main/java/pl/WojtekMalka/csvReader/service/ReaderService.java
+++ src/main/java/pl/WojtekMalka/csvReader/service/ReaderService.java
@@ -5,48 +5,40 @@ import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import pl.WojtekMalka.csvReader.dictionary.ClientDictionary;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.EnumMap;
 import java.util.List;
 
 @Service
+@NoArgsConstructor
 public class ReaderService {
 
-    /*TODO -logowanie*/
+    private static Logger logger = LoggerFactory.getLogger(ReaderService.class);
 
     private static final String fileSourceDirectory = "C:\\Users\\WojtekM\\Repozytorium\\testInput.csv";
 
-    public ReaderService() {
-    }
-
-    public ReaderService(String fileSource) {
+    public ReaderService(String s) {
     }
 
     public static List<String[]> readFile(String fileSourceDirectory) throws IOException, CsvException {
+        logger.info("ReaderService service input: {}", fileSourceDirectory);
         CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
+
         try (CSVReader reader = new CSVReaderBuilder(
                 new FileReader(fileSourceDirectory))
                 .withCSVParser(csvParser)
                 .withSkipLines(1)
                 .build()) {
-            return reader.readAll();
+            List<String[]> result = reader.readAll();
+            logger.info("ReaderService service output: {}", result);
+
+            return result;
         }
         /*TODO -stworzyć błąd NonFileFound, obsługa błędu */
-    }
-
-    public static EnumMap<ClientDictionary, String> getOneClientData(List<String[]> input, Integer lineNumber) {
-        String[] strings = input.get(lineNumber);
-        EnumMap<ClientDictionary, String> oneLineInMap = new EnumMap<>(ClientDictionary.class);
-
-        oneLineInMap.put(ClientDictionary.FIRST_NAME, strings[0]);
-        oneLineInMap.put(ClientDictionary.LAST_NAME, strings[1]);
-        oneLineInMap.put(ClientDictionary.BIRTH_DATE, strings[2]);
-        oneLineInMap.put(ClientDictionary.PHONE_NO, strings[3]);
-
-        return oneLineInMap;
     }
 }
