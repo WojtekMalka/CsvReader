08c294aa501d8013a19ce16713f3c98f483173fd
WojtekMalka <malka_wojciech@wp.pl>
2021-09-21 19:01:42 +0200

    Loggers

--- src/main/java/pl/WojtekMalka/csvReader/service/ClientDataParser.java
+++ src/main/java/pl/WojtekMalka/csvReader/service/ClientDataParser.java
@@ -1,15 +1,22 @@
 package pl.WojtekMalka.csvReader.service;
 
 import org.apache.commons.text.WordUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 @Service
 public class ClientDataParser {
+
+    private static Logger logger = LoggerFactory.getLogger(ReaderService.class);
+
     public static void parse(String input) {
+        logger.info("ClientDataParser input: {}", input);
         cleanWhiteSpacesAndFillsUpEmpty(input);
         correctLettersSizeInString(input);
+        logger.info("ClientDataParser output: {}", input);
     }
 
     private static void correctLettersSizeInString(String input) {
