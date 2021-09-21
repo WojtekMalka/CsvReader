6372f981097b1cd0d2d69eebe29887f285714891
WojtekMalka <malka_wojciech@wp.pl>
2021-09-21 19:33:35 +0200

    Home controller crated

--- src/main/java/pl/WojtekMalka/csvReader/controller/HomeController.java
+++ src/main/java/pl/WojtekMalka/csvReader/controller/HomeController.java
@@ -4,8 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
 @RequestMapping("/")
@@ -14,8 +25,30 @@ public class HomeController {
 
     private static final String PAGE_HOME = "home";
 
+    private static final String UPLOAD_DIR = "./uploads/";
+
     @GetMapping("/")
     String showHome(Model model) {
         return PAGE_HOME;
     }
+
+    @PostMapping("/loadFile")
+    String loadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) {
+        if (file.isEmpty()) {
+            attributes.addFlashAttribute("message", "Please select a file to upload.");
+        }
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        attributes.addFlashAttribute("message", "You successfully uploaded " + fileName + '!');
+
+        return "redirect:/";
+    }
+}
 }
