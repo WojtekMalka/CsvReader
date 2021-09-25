957edc3dae5b612806efef4a37d0d54db4e8f55d
WojtekMalka <malka_wojciech@wp.pl>
2021-09-25 15:45:44 +0200

    Client controller changed to REST

--- src/main/java/pl/WojtekMalka/csvReader/controller/ClientController.java
+++ src/main/java/pl/WojtekMalka/csvReader/controller/ClientController.java
@@ -1,27 +1,32 @@
 package pl.WojtekMalka.csvReader.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.WojtekMalka.csvReader.entity.Client;
 import pl.WojtekMalka.csvReader.repository.ClientRepository;
 
+import java.util.List;
+
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/client")
 public class ClientController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
-
-    private static final String PAGE_CLIENT_VIEW = "client/view";
-
+    
     private final ClientRepository clientRepository;
 
-    @GetMapping()
-    String getAllUserViewPage(Model model) {
-        model.addAttribute("client", clientRepository.findAll());
-        return PAGE_CLIENT_VIEW;
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping("/getAll")
+    ResponseEntity<List<Client>> readAllClients() {
+        if (clientRepository.findAll().isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(clientRepository.findAll());
     }
 }
--- src/main/java/pl/WojtekMalka/csvReader/entity/Client.java
+++ src/main/java/pl/WojtekMalka/csvReader/entity/Client.java
@@ -1,6 +1,5 @@
 package pl.WojtekMalka.csvReader.entity;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
