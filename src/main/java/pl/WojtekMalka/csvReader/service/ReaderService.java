b5649893cf28cf9c4c841b29b7e19221ea14300e
WojtekMalka <malka_wojciech@wp.pl>
2021-09-25 16:48:01 +0200

    LoadController changed to rest load controller

--- src/main/java/pl/WojtekMalka/csvReader/service/ReaderService.java
+++ src/main/java/pl/WojtekMalka/csvReader/service/ReaderService.java
@@ -1,44 +1,36 @@
 package pl.WojtekMalka.csvReader.service;
 
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvException;
 import lombok.NoArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
 @NoArgsConstructor
 public class ReaderService {
-
     private static Logger logger = LoggerFactory.getLogger(ReaderService.class);
 
-    private static final String fileSourceDirectory = "C:\\Users\\WojtekM\\Repozytorium\\Test.csv";
+    public static List<String[]> readFile(URL sourceURL) {
+        logger.info("ReaderService service input: {}", sourceURL.toString());
+        CSVFormat csvFormat = CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase();
 
-    public ReaderService(String s) {
-    }
-
-    public static List<String[]> readFile(String fileSourceDirectory) throws IOException, CsvException {
-        logger.info("ReaderService service input: {}", fileSourceDirectory);
-        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
-
-        try (CSVReader reader = new CSVReaderBuilder(
-                new FileReader(fileSourceDirectory))
-                .withCSVParser(csvParser)
-                .withSkipLines(1)
-                .build()) {
-            List<String[]> result = reader.readAll();
-            logger.info("ReaderService service output: {}", result);
-
-            return result;
+        try (CSVParser csvParser = CSVParser.parse(sourceURL, StandardCharsets.UTF_8, csvFormat)) {
+            for (CSVRecord csvRecord : csvParser) {
+                String firstName = csvRecord.get("First Name");
+                String lastName = csvRecord.get("Last Name");
+                String email = csvRecord.get("Email");
+                String phoneNumber = csvRecord.get("Phone Number");
+            }
+            /*TODO -stworzyć błąd NonFileFound, obsługa błędu */
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        /*TODO -stworzyć błąd NonFileFound, obsługa błędu */
     }
-}
