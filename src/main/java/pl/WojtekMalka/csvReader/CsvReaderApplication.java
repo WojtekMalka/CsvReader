9db02e11fef7f2eaa0b3e76d55488e5524f2caf5
WojtekMalka <malka_wojciech@wp.pl>
2021-09-25 18:16:32 +0200

    Changes in pom:

--- src/main/java/pl/WojtekMalka/csvReader/CsvReaderApplication.java
+++ src/main/java/pl/WojtekMalka/csvReader/CsvReaderApplication.java
@@ -1,15 +1,12 @@
 package pl.WojtekMalka.csvReader;
 
-import com.opencsv.exceptions.CsvException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class CsvReaderApplication {
 
-    public static void main(String[] args) throws IOException, CsvException {
+    public static void main(String[] args) {
         SpringApplication.run(CsvReaderApplication.class, args);
     }
 }
