5d5eb58a1c5a303d9a09ce3611d17a45854aa1dd
WojtekMalka <malka_wojciech@wp.pl>
2021-09-30 16:36:09 +0200

    Error push

--- src/main/java/pl/WojtekMalka/csvReader/service/ClientDataParser.java
+++ src/main/java/pl/WojtekMalka/csvReader/service/ClientDataParser.java
@@ -4,31 +4,42 @@ import org.apache.commons.lang3.text.WordUtils;
 
 public class ClientDataParser {
     private static final String INCORRECT_DATE_FORMAT = "^\\d{4}\\.([1-9])\\.([1-9])$";
-    public static final String S_0 = "0";
-    public static final String REGEX_DOT = "\\.";
-    public static final String DOT = ".";
+    private static final String S_0 = "0";
+    private static final String REGEX_DOT = "\\.";
+    private static final String DOT = ".";
 
     public static String parseName(String input) {
-        input.trim().toLowerCase();
-        WordUtils.capitalize(input);
-        return input;
+        return WordUtils.capitalize(input.trim().toLowerCase());
     }
 
     public static String parseDate(String input) {
         if (input.matches(INCORRECT_DATE_FORMAT)) {
             String[] splitedInput = input.split(REGEX_DOT);
-            String correctMonth = "";
-            String correctDay = "";
+            String correctMonth;
+            String correctDay;
 
-            if (splitedInput[1].length() == 1) {
-                correctMonth += S_0 + splitedInput[1].toString();
-            }
-            if (splitedInput[2].length() == 1) {
-                correctDay += S_0 + splitedInput[2].toString();
-            }
-            StringBuilder sb = new StringBuilder();
-            return sb.append(splitedInput[0]).append(DOT).append(correctMonth).append(DOT).append(correctDay).toString();
+            correctMonth = getCorrectFormat(splitedInput[1]);
+            correctDay = getCorrectFormat(splitedInput[2]);
+
+            return correctDateBuilder(splitedInput[0], correctMonth, correctDay);
         }
         return input;
+    }
+
+    private static String correctDateBuilder(String str, String correctMonth, String correctDay) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(str)
+                .append(DOT)
+                .append(correctMonth)
+                .append(DOT)
+                .append(correctDay)
+                .toString();
+    }
+
+    private static String getCorrectFormat(String splitedInput) {
+        if (splitedInput.length() == 1) {
+            return S_0 + splitedInput;
+        }
+        return splitedInput;
     }
 }
