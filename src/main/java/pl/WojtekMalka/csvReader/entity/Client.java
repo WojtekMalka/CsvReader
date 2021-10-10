444e602c5150cfdee7d2528b9e4229dd15c2e306
WojtekMalka <malka_wojciech@wp.pl>
2021-10-10 18:13:11 +0200

    Client entitie validator regex

--- src/main/java/pl/WojtekMalka/csvReader/entity/Client.java
+++ src/main/java/pl/WojtekMalka/csvReader/entity/Client.java
@@ -5,13 +5,13 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static java.time.temporal.ChronoUnit.YEARS;
 import static pl.WojtekMalka.csvReader.message.EntityValidatorMessages.EMPTY_VALUE;
-import static pl.WojtekMalka.csvReader.message.EntityValidatorMessages.WRONG_LENGTH;
+import static pl.WojtekMalka.csvReader.message.EntityValidatorMessages.WRONG_PHONE_NUMBER;
 
 @Entity
 @Data
@@ -31,8 +31,7 @@ public class Client {
     @NotEmpty(message = EMPTY_VALUE)
     private String birth_date;
 
-    @Column(unique = true)
-    @Size(min = 9, max = 9, message = WRONG_LENGTH)
+    @Pattern(regexp = "(^[0-9]+$|^$)", message = WRONG_PHONE_NUMBER)
     private String phone_no;
     private Long client_age;
 
--- src/main/java/pl/WojtekMalka/csvReader/message/EntityValidatorMessages.java
+++ src/main/java/pl/WojtekMalka/csvReader/message/EntityValidatorMessages.java
@@ -2,5 +2,5 @@ package pl.WojtekMalka.csvReader.message;
 
 public class EntityValidatorMessages {
     public static final String EMPTY_VALUE = "Value couldn't be empty";
-    public static final String WRONG_LENGTH = "Value has wrong lenght";
+    public static final String WRONG_PHONE_NUMBER = "Wrong phone number";
 }
